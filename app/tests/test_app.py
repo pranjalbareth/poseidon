56f177021afa07ba480dd9f8c8d0a50b21547179
Pranjal Bareth <pranjalnirmala@gmail.com>
2025-09-11 02:55:01 +0530

    minor fixes

--- app/tests/test_app.py
+++ app/tests/test_app.py
@@ -1,7 +1,7 @@
-from app import app
+from app.app import app 
 
 def test_index():
     client = app.test_client()
-    resp = client.get("/")
-    assert resp.status_code == 200
-    assert resp.get_json() == {"message": "hello from Poseidon CI/CD!"}
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.get_json() == {"message": "hello from Poseidon CI/CD demo!"}
