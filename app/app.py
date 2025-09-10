c3ff7d595e8362a4d9e9758c049ee5b8b661677a
Pranjal Bareth <pranjalnirmala@gmail.com>
2025-09-11 02:28:11 +0530

    Added Prometheus & Grafana Support

--- app/app.py
+++ app/app.py
@@ -1,10 +1,15 @@
 from flask import Flask, jsonify
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
+import prometheus_client
 
 app = Flask(__name__)
+REQUEST_COUNT = Counter('poseidon_requests_total', 'Total requests')
 
 @app.route("/")
 def index():
-    return jsonify({"message": "hello from Poseidon CI/CD!"})
+    REQUEST_COUNT.inc()
+    return jsonify({"message": "hello from Poseidon CI/CD demo!"})
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+@app.route("/metrics")
+def metrics():
+    return generate_latest(), 200, {'Content-Type': CONTENT_TYPE_LATEST}
